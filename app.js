26c93c403c32097a455753d2ae4a4066626720a5
ping8601 <wanping86701@gmail.com>
2022-10-07 19:41:11 +0800

    add edit restaurant function

--- app.js
+++ app.js
@@ -56,9 +56,36 @@ app.post('/restaurants', (req, res) => {
     .catch(error => console.error(error))
 })
 
-app.get('/restaurants/new', (req, res) => {
-  res.render('new')
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = req.body.name
+      restaurant.category = req.body.category
+      restaurant.image = req.body.image
+      restaurant.location = req.body.location
+      restaurant.phone = req.body.phone
+      restaurant.google_map = req.body.google_map
+      restaurant.rating = req.body.rating
+      restaurant.description = req.body.description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.error(error))
 })
+
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(error => console.error(error))
+})
+
 
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
