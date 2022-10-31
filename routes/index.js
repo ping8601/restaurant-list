0eb20ee0ee28e9dbc47431eb138c629800b620e8
ping8601 <wanping86701@gmail.com>
2022-10-31 19:30:10 +0800

    add user authenticator in user and home route

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
+
 const users = require('./modules/users')
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
-router.use('/restaurants', restaurants)
-router.use('/', home)
+router.use('/restaurants', authenticator, restaurants)
+router.use('/', authenticator, home)
 
 module.exports = router
