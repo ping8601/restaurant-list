f86321b989dbc612bc121de19f1510a49a459fe4
ping8601 <wanping86701@gmail.com>
2022-10-31 16:55:47 +0800

    add user registration feature

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const { text } = require('express')
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema()
+const Schema = mongoose.Schema
 
 const userSchema = new Schema({
   name: {
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
 router.post('/login', (req, res) => {
-
+  const { email, password }  = req.body
+  User.findOne({ email })
+  .then(user => {
+    if (!user) {
+      return res.render('login')
+    }
+  })
+  .then()
 })
 
 router.get('/register', (req, res) => {
@@ -14,7 +23,27 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-
+  const {name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+  .then(user => {
+    if (user) {
+      console.log('User already exists.')
+      return res.render('register', {
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    } else {
+      return User.create({
+        name,
+        email,
+        password
+      })
+      .then(() => res.redirect('/'))
+      .catch(error => console.error)
+    }
+  }) 
 })
 
 module.exports = router
