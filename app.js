0669d59e13f3375b1fad1244529d258b5edf6c31
ping8601 <wanping86701@gmail.com>
2022-10-31 17:03:40 +0800

    import session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const session = require('express-session')
 
 const routes = require('./routes')
 require('./config/mongoose')
@@ -21,6 +22,13 @@ app.use(express.static('public'))
 
 // use method-override
 app.use(methodOverride('_method'))
+
+// use session
+app.use(session({
+  secret: 'MySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // set routes
 app.use(routes)
